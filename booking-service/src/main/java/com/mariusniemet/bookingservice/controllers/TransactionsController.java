fb755692221e25c6fa762a2866e7ae71ffb91116
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-05-25 23:52:48 +0000

    implemented remove, findOne endpoints

--- booking-service/src/main/java/com/mariusniemet/bookingservice/controllers/TransactionsController.java
+++ booking-service/src/main/java/com/mariusniemet/bookingservice/controllers/TransactionsController.java
@@ -4,13 +4,15 @@ package com.mariusniemet.bookingservice.controllers;
 import com.mariusniemet.bookingservice.dtos.TransactionCreateDto;
 import com.mariusniemet.bookingservice.entities.Transaction;
 import com.mariusniemet.bookingservice.services.TransactionsService;
+import jakarta.ws.rs.NotFoundException;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/transactions")
@@ -24,10 +26,31 @@ public class TransactionsController {
 
 
     @PostMapping
-    public ResponseEntity<Transaction> create(@RequestBody TransactionCreateDto createDto){
+    public ResponseEntity<Transaction> create(@RequestBody TransactionCreateDto createDto) throws BadRequestException {
         Transaction result = this.service.create(createDto);
 
         return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> findAll(@RequestParam(required = false) Integer userId, @RequestParam(required = false) Integer evenId) {
+        List<Transaction> result = this.service.findAll(userId, evenId);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> findOne(@PathVariable Long id){
+        Transaction result = this.service.findById(id);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Transaction> remove(@PathVariable Long id) throws NotFoundException {
+        Transaction tr = this.service.remove(id);
+
+        return new ResponseEntity<>(tr, HttpStatus.OK);
     }
 
 }
--- booking-service/src/main/java/com/mariusniemet/bookingservice/services/TransactionsService.java
+++ booking-service/src/main/java/com/mariusniemet/bookingservice/services/TransactionsService.java
@@ -3,11 +3,15 @@ package com.mariusniemet.bookingservice.services;
 import com.mariusniemet.bookingservice.dtos.TransactionCreateDto;
 import com.mariusniemet.bookingservice.entities.Transaction;
 import com.mariusniemet.bookingservice.repositories.ITransaction;
+import jakarta.ws.rs.NotFoundException;
+import org.apache.coyote.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionsService {
@@ -20,27 +24,61 @@ public class TransactionsService {
 
     // create => update solded tickets number in the Ticket table
 
-    public Transaction create(TransactionCreateDto createDto){
-
-        Transaction toCreate = new Transaction(createDto.getUserId(), createDto.getQuantity(), createDto.getTicketId());
-        Transaction result = null;
+    public Transaction create(TransactionCreateDto createDto) throws BadRequestException {
+        logger.info("Creating a transaction ...");
+        Transaction toCreate = new Transaction(createDto.getUserId(), createDto.getQuantity(), createDto.getTicketId(), createDto.getEventId());
 
         toCreate.setTransactionDate(new Date());
         toCreate.setTotalAmount(createDto.getQuantity() * createDto.getTicketPrice());
 
         try {
-            result = this.iTransaction.save(toCreate);;
-            logger.info("Transaction successfully created");
+            Transaction result = this.iTransaction.save(toCreate);;
+            logger.info("Transaction successfully created {}", result);
+            return  result;
         }catch (Exception e){
             logger.error("Failed to create the transaction", e);
+            throw new BadRequestException("Failed to create the transaction");
+        }
+    }
+
+    public Transaction findById(Long id) throws NotFoundException{
+        logger.info("Finding the transaction...");
+        Optional<Transaction> transaction = this.iTransaction.findById(id);
+
+        if(transaction.isEmpty()){
+            logger.error("Failed to fetch the Transaction {}", id);
+            throw new NotFoundException("Failed to fetch the transaction");
+        }
+        logger.info("Transaction found");
+        return transaction.get();
+    }
+
+
+    // findAll => by user or by events
+    public List<Transaction> findAll(Integer userId, Integer eventId){
+        List<Transaction> result = null;
+
+        try{
+            result = this.iTransaction.findAllByUserIdAndEventId(userId, eventId);
+            logger.info("Transactions successfully fetched");
+        }catch (Exception e){
+            logger.error("Failed to fetch transactions", e);
         }
 
         return result;
     }
 
-    // update
+    public Transaction remove(Long id) throws NotFoundException {
+        logger.info("Removing a transaction {}", id);
+        Optional<Transaction> toRemove = this.iTransaction.findById(id);
 
-    // findAll => by user or by events
+        if(toRemove.isEmpty()){
+            logger.error("Failed to fetch the transaction {}", id);
+            throw  new NotFoundException("Failed to fetch the transaction");
+        }
 
-    // delete
+        this.iTransaction.delete(toRemove.get());
+        logger.info("Transaction removed");
+        return  toRemove.get();
+    }
 }
