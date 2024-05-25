9e42d03123c2d071b19de7171509b0d9bc17f587
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-05-25 23:54:04 +0000

    added the findAllByUserIdAndEventId in the ITransaction

--- booking-service/src/main/java/com/mariusniemet/bookingservice/repositories/ITransaction.java
+++ booking-service/src/main/java/com/mariusniemet/bookingservice/repositories/ITransaction.java
@@ -2,6 +2,16 @@ package com.mariusniemet.bookingservice.repositories;
 
 import com.mariusniemet.bookingservice.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ITransaction extends JpaRepository<Transaction, Long> {
+
+    @Query("SELECT t FROM Transaction t WHERE " +
+            "(:userId IS NULL OR t.userId = :userId) AND " +
+            "(:eventId IS NULL OR t.eventId = :eventId)")
+    List<Transaction> findAllByUserIdAndEventId(@Param("userId") Integer userId,
+                                                @Param("eventId") Integer eventId);
 }
