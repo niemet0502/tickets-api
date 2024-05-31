2c49f45b3c7c7bd4476f31d12102745783bdd718
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-05-31 00:44:38 +0000

    added the id column in the event db table

--- events-service/src/main/java/com/mariusniemet/eventsservice/entities/Event.java
+++ events-service/src/main/java/com/mariusniemet/eventsservice/entities/Event.java
@@ -2,14 +2,16 @@ package com.mariusniemet.eventsservice.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.Date;
 
 @Entity
 public class Event {
-    private @Id
-    @GeneratedValue Long id;
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
 
     private String name;
 
@@ -24,14 +26,6 @@ public class Event {
         this.address = address;
         this.name = name;
         this.date = date;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public void setName(String name) {
