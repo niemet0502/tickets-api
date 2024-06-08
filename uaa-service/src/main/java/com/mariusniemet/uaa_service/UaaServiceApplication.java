3eda9c7ba7171d67632cbe8693af4d7861754bc6
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-06-08 20:11:21 +0000

    init the uaa service project

---
+++ uaa-service/src/main/java/com/mariusniemet/uaa_service/UaaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mariusniemet.uaa_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaaServiceApplication.class, args);
+	}
+
+}
---
+++ uaa-service/src/test/java/com/mariusniemet/uaa_service/UaaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariusniemet.uaa_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
